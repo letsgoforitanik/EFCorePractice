cd84d324f2eb117ffd9643a4ed51d8ea423d88a4
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2025-01-09 20:03:44 +0530

    Use DataAnnotations on model to override EF Core conventions

--- EFCore.API/Models/Movie.cs
+++ EFCore.API/Models/Movie.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EFCore.API.Models;
 
+[Table("Pictures")]
 public class Movie
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
+    [Key]
+    public int Identifier { get; set; }
+
+    [MaxLength(128)]
+    [Column(TypeName = "varchar")]
+    public string Title { get; set; } = default!;
+
+    [Column(TypeName = "date")]
     public DateTime ReleaseDate { get; set; }
+
+    [Column("Plot", TypeName = "varchar(max)")]
     public string? Synopsis { get; set; }
 }
